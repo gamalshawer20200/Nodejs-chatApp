a78321437a338b7e47151bbb2d95c5c581f2aabe
Jemi <gamalshawer20200@gmail.com>
2019-05-08 18:15:55 +0200

    modified sockets for deploy

--- server/server.js
+++ server/server.js
@@ -1,18 +1,15 @@
 const path = require('path');
-const http = require('http')
-const express = require('express')
-const socketIO = require('socket.io')
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
-const publicPath = path.join(__dirname, '../public')
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-var app = express()
-var server = http.createServer(app)
+var app = express();
+var server = http.createServer(app);
 var io = socketIO(server);
-// console.log(__dirname+ '/../public')
-// console.log(publicPath)
 
-
-app.use(express.static(publicPath))
+app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {  //allows you to listen to an event and do something when this event happens
     console.log('New User Connected')

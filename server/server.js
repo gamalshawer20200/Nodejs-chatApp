8ab81419e326558c16f7fb22f8486c5049eb21a0
Jemi <gamalshawer20200@gmail.com>
2019-05-08 16:41:56 +0200

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -17,11 +17,29 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {  //allows you to listen to an event and do something when this event happens
     console.log('New User Connected')
 
+    // socket.emit('newEmail', {
+    //     from: 'newEmail@example.com',
+    //     text: 'Hey, what is going on',
+    //     createdAt: 123
+    // })
+    socket.emit('newMessage', {
+        from: 'newMessage@example.com',
+        text: 'Hello every one to the chat-room from SERVER !'
+    })
+
+    // socket.on('createEmail', (newEmail) => {
+    //     console.log('createEmail', newEmail)
+    //     // console.log('This E-mail well sent to -> ', newEmail.to)
+    // })
+
+    socket.on('createMessage', (message) => {
+        console.log('Message',message)
+    })
+
     socket.on('disconnect', (socket) => {
         console.log('One User Disconnected !')
     })
 })
-
 
 
 server.listen(port, () => {

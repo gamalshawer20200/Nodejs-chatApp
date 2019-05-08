e190c891a8b619e23ad4dafcaaee664c6ce4d6a8
Jemi <gamalshawer20200@gmail.com>
2019-05-08 18:20:56 +0200

    modified sockets for deploy

--- server/server.js
+++ server/server.js
@@ -11,24 +11,40 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection', (socket) => {
-  console.log('New user connected');
+io.on('connection', (socket) => {  //allows you to listen to an event and do something when this event happens
+    console.log('New User Connected')
 
-  socket.emit('newMessage', {
-    from: 'John',
-    text: 'See you then',
-    createdAt: 123123
-  });
+    socket.emit('newMessage', {
+        from: 'serverGM@example.com',
+        text: 'Welcome to chat-room from SERVER !',
+        createdAt: new Date().toLocaleDateString()
+    })
+    socket.broadcast.emit('newArrival', {
+        from: 'Admin',
+        text: `New user joined`,
+        createdAt: new Date().toLocaleDateString()
+    })
 
-  socket.on('createMessage', (message) => {
-    console.log('createMessage', message);
-  });
+    socket.on('createMessage', (message) => {
+        console.log('Message', message)
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().toLocaleDateString()
+        // })
+        socket.broadcast.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().toLocaleDateString()
+        })
+    })
 
-  socket.on('disconnect', () => {
-    console.log('User was disconnected');
-  });
-});
+    socket.on('disconnect', (socket) => {
+        console.log('One User Disconnected !')
+    })
+})
+
 
 server.listen(port, () => {
-  console.log(`Server is up on ${port}`);
-});
+    console.log(`Server starting at port ${port} ...`)
+})
